74684806849eda4b0c0278725164098376659116
Zahid Hussain <zahidhussain909@gmail.com>
2023-02-24 14:30:01 +0530

    added pid

--- thruster_fin.py
+++ thruster_fin.py
@@ -68,7 +68,6 @@ def sig(value):
 def forward():
     con.update()
 
-
     up=map_values_int(con.getThrottle(),1900,1100)
     move=map_values_int(con.getPitch(),1900,1100)
     turn=sig(con.getYaw())
@@ -82,13 +81,7 @@ def map_values_dynamic(value, max_val, min_val):
     scaled_value = (value + 1) / 2 * (max_val - min_val) + min_val
     return float(scaled_value)
 
-
-start_time=0
-
-while True:
-    start_time = time.time()
-    forward()
-    con.update()
+def pid():
     target_depth = map_values_dynamic(con.getThrottle(), 5, 0)
     if sensor.read():
         currentDepth = sensor.depth()
@@ -119,7 +112,52 @@ while True:
         pi.set_servo_pulsewidth(thruster_1, pwmup)
         pi.set_servo_pulsewidth(thruster_2, pwmup)
         previous_error = error  
-
     else:
         print("Sensor read failed!")
         exit(1)
+
+start_time=0
+
+while True:
+    start_time = time.time()
+    con.update()
+    #forward()
+    up = map_values_dynamic(con.getThrottle(),5,0)
+    move = map_values(con.getPitch())
+    turn = sig(con.getYaw())
+    #con.update()
+    toggle = con.getAux()
+    prev=1
+    curr=toggle
+    if(prev==toggle):
+        pi.set_servo_pulsewidth(thruster_1, 1500)
+        pi.set_servo_pulsewidth(thruster_2, 1500)
+        pi.set_servo_pulsewidth(thruster_3, 1500)
+        pi.set_servo_pulsewidth(thruster_4, 1500)
+        print("thruster off")
+        break
+    else:
+        if turn == 1500 and move == 1500 and up==0:
+            pi.set_servo_pulsewidth(thruster_1, 1500)
+            pi.set_servo_pulsewidth(thruster_2, 1500)
+            pi.set_servo_pulsewidth(thruster_3, 1500)
+            pi.set_servo_pulsewidth(thruster_4, 1500)
+            print("thruster stop")
+            print(move)
+        elif turn!=1500: 
+            pi.set_servo_pulsewidth(thruster_2, turn+50)
+            pi.set_servo_pulsewidth(thruster_3, turn-50)
+            pid()
+            print("Thruster 1: ", turn+50)
+            print("Thruster 2: ", turn-50)
+        elif move!=1500:
+            pi.set_servo_pulsewidth(thruster_2, move)
+            pi.set_servo_pulsewidth(thruster_3, move)
+            pid()
+            print("forward thrust: ", move)
+        elif up!=0:
+            pid()
+            #pi.set_servo_pulsewidth(thruster_1, up)
+            #pi.set_servo_pulsewidth(thruster_4, up)
+            print("up thrust: ", up)
+            break
